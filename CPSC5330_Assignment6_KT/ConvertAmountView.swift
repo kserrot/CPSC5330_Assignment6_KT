a669b4799b55daad2501d964d197be32c64214f1
Kaique Torres <kaique.serrot@gmail.com>
2024-02-17 15:30:14 -0600

    Skeleton

---
+++ CPSC5330_Assignment6_KT/ConvertAmountView.swift
@@ -0,0 +1,29 @@
+//
+//  ConvertAmountViewController.swift
+//  CPSC5330_Assignment6_KT
+//
+//  Created by Kaique Torres on 2/17/24.
+//
+
+import UIKit
+
+class ConvertAmountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CPSC5330_Assignment6_KT/ConvertLogic.swift
@@ -0,0 +1,13 @@
+//
+//  ConvertLogic.swift
+//  CPSC5330_Assignment6_KT
+//
+//  Created by Kaique Torres on 2/17/24.
+//
+
+import Foundation
+
+struct ConvertLogic {
+    
+    //var 
+}
