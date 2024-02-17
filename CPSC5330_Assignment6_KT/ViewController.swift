02641391cd675d713eec90342d6b39295e3a96b2
Kaique Torres <kaique.serrot@gmail.com>
2024-02-17 13:19:28 -0600

    Initial commit

---
+++ CPSC5330_Assignment6_KT/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  CPSC5330_Assignment6_KT
+//
+//  Created by Kaique Torres on 2/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func amountInUSD(_ sender: UITextField) {
+        
+    }
+    
+    @IBAction func euroCurrency(_ sender: UISwitch) {
+        
+    }
+    
+    @IBAction func yenCurrency(_ sender: UISwitch) {
+        
+    }
+    
+    @IBAction func pesosCurrency(_ sender: UISwitch) {
+        
+    }
+    
+    @IBAction func rubleCurrency(_ sender: UISwitch) {
+        
+    }
+    
+    
+    @IBAction func convertButton(_ sender: UIButton) {
+        
+    }
+    
+}
+
